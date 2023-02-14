bd64a7ce2a3c59f64232f6a7471c4a9b28f96d9c
曹骏 <837087867@qq.com>
2023-02-14 11:45:57 +0800

    refactor: update basic_types

--- lib/utils/basic_types.dart
+++ lib/utils/basic_types.dart
@@ -6,3 +6,7 @@ typedef PickerFuture<T> = Future<T?>? Function(
   BuildContext context,
   T? initailValue,
 );
+
+typedef ValueChangedForResult<T> = bool Function(T value);
+
+typedef VoidCallbackForResult = bool Function();
