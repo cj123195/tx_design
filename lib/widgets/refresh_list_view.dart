dca2af7a1a555624a8473be418a9ffa9c9cf7f75
曹骏 <837087867@qq.com>
2023-02-14 13:37:52 +0800

    feat: add refresh_list_view

---
+++ lib/widgets/refresh_list_view.dart
@@ -0,0 +1,101 @@
+import 'package:easy_refresh/easy_refresh.dart';
+import 'package:flutter/material.dart';
+
+/// 列表刷新组件
+class RefreshListView extends EasyRefresh {
+  const RefreshListView({
+    required super.child,
+    super.key,
+    super.controller,
+    super.header,
+    super.footer,
+    super.onRefresh,
+    super.onLoad,
+    super.spring,
+    super.frictionFactor,
+    super.notRefreshHeader,
+    super.notLoadFooter,
+    super.simultaneously = false,
+    super.noMoreRefresh = false,
+    super.noMoreLoad = false,
+    super.resetAfterRefresh = true,
+    super.refreshOnStart = false,
+    super.refreshOnStartHeader,
+    super.callRefreshOverOffset = 20,
+    super.callLoadOverOffset = 20,
+    super.fit = StackFit.loose,
+    super.clipBehavior = Clip.hardEdge,
+    super.scrollBehaviorBuilder,
+    super.scrollController,
+  });
+
+  RefreshListView.builder({
+    required int itemCount,
+    required NullableIndexedWidgetBuilder itemBuilder,
+    EdgeInsetsGeometry? padding,
+    super.key,
+    super.controller,
+    super.header,
+    super.footer,
+    super.onRefresh,
+    super.onLoad,
+    super.spring,
+    super.frictionFactor,
+    super.notRefreshHeader,
+    super.notLoadFooter,
+    super.simultaneously = false,
+    super.noMoreRefresh = false,
+    super.noMoreLoad = false,
+    super.resetAfterRefresh = true,
+    super.refreshOnStart = false,
+    super.refreshOnStartHeader,
+    super.callRefreshOverOffset = 20,
+    super.callLoadOverOffset = 20,
+    super.fit = StackFit.loose,
+    super.clipBehavior = Clip.hardEdge,
+    super.scrollBehaviorBuilder,
+    super.scrollController,
+  }) : super(
+          child: ListView.builder(
+            itemBuilder: itemBuilder,
+            itemCount: itemCount,
+            padding: padding,
+          ),
+        );
+
+  RefreshListView.separated({
+    required int itemCount,
+    required NullableIndexedWidgetBuilder itemBuilder,
+    IndexedWidgetBuilder? separatorBuilder,
+    EdgeInsetsGeometry? padding,
+    super.key,
+    super.controller,
+    super.header,
+    super.footer,
+    super.onRefresh,
+    super.onLoad,
+    super.spring,
+    super.frictionFactor,
+    super.notRefreshHeader,
+    super.notLoadFooter,
+    super.simultaneously = false,
+    super.noMoreRefresh = false,
+    super.noMoreLoad = false,
+    super.resetAfterRefresh = true,
+    super.refreshOnStart = false,
+    super.refreshOnStartHeader,
+    super.callRefreshOverOffset = 20,
+    super.callLoadOverOffset = 20,
+    super.fit = StackFit.loose,
+    super.clipBehavior = Clip.hardEdge,
+    super.scrollBehaviorBuilder,
+    super.scrollController,
+  }) : super(
+            child: ListView.separated(
+          itemBuilder: itemBuilder,
+          itemCount: itemCount,
+          padding: padding,
+          separatorBuilder:
+              separatorBuilder ?? (context, index) => const SizedBox(height: 8),
+        ));
+}
